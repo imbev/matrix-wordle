90fe2142c4b1fc50d5b3117cbc439ce7e09422aa
KrazyKirby99999 <krazykirby99999@gmail.com>
2022-02-15 15:58:47 +0000

    Add wordle check_guess function

--- wordle.py
+++ wordle.py
@@ -2,6 +2,9 @@ import csv
 import datetime
 import os
 
+GREEN = '🟩'
+YELLOW = '🟨'
+BLACK = '⬛'
 
 def get_daily():
     now = datetime.datetime.now()
@@ -12,3 +15,25 @@ def get_daily():
         for row in reader:
             if (row[0], row[1], row[2]) == (month, day, year):
                 return row[5]
+
+def check_guess(guess: str, answer: str) -> list:
+    guess = guess.upper()
+
+    with open(os.path.join('resources', 'wordle_key.csv'), newline='') as f:
+        reader = csv.reader(f, delimiter=',')
+        valid = False
+        for row in reader:
+            if row[5] == guess:
+                valid = True
+        if not valid:
+            return []
+
+    check = []
+    for i in range(5):
+        if guess[i] == answer[i]:
+            check.append(GREEN)
+        elif guess[i] in answer:
+            check.append(YELLOW)
+        else:
+            check.append(BLACK)
+    return check
