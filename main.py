df0090419739a9f0ad766bf0adf098978f28dd8c
KrazyKirby99999 <krazykirby99999@gmail.com>
2022-02-18 02:46:34 +0000

    Add guess_word command handler

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
             "An implementation of the popular Wordle game for the Matrix Protocol.\n"
             "Commands:\n"
             "help, h - Display help message\n"
-            "start, s - Start a new Wordle game")
+            "start, s - Start a new Wordle")
         await bot.api.send_markdown_message(room.room_id, response)
     
     @bot.listener.on_message_event
@@ -44,9 +44,54 @@ if __name__ == '__main__':
         state[user] = 0
         response = ("Starting new Wordle game!\n\n"\
                     "Guess a 5 letter word:\n\n"
-                    "```         ```"
+                    "```- - - - -```"
         )
         await bot.api.send_markdown_message(room.room_id, response)
         s.save_state(state)
+
+    @bot.listener.on_message_event
+    @s.ensure_state
+    async def guess_word(room, message, state):
+        match = botlib.MessageMatch(room, message, bot, prefix)
+        if not (match.prefix() and (match.command('guess') or match.command('g'))):
+            return
+        
+        user = message.sender
+
+        result = w.check_guess(match.args()[0], w.get_daily())
+        response = f"{'   '.join(x for x in match.args()[0])}\n{''.join(x for x in result)}"
+        
+        
+        if not (user in state.keys()):
+            response = f"Please start a new Wordle game with \"{prefix}start\" before guessing!"
+            await bot.api.send_text_message(room.room_id, response)
+            return
+        
+        if not state[user] in range(0, 5):
+            response = f"{response}\nOut of guesses!\n The answer may be revealed with \"{prefix}answer\"."
+            await bot.api.send_markdown_message(room.room_id, response)
+            state.pop(user)
+            s.save_state(state)
+            return
+        
+        if not (
+            len(match.args()) == 1 and
+            w.check_guess(
+                match.args()[0], w.get_daily()
+                )):
+            response = "Invalid guess. Please guess a valid 5 letter word"
+            await bot.api.send_text_message(room.room_id, response)
+            return
+        
+        if result == list(w.GREEN*5):
+            response = f"{response}\nThe answer was {w.get_daily()}. You Won in {state[user]+1} tries!"
+            await bot.api.send_text_message(room.room_id, response)
+            state.pop(user)
+            s.save_state(state)
+            return
+
+        state[user] = state[user] + 1
+        s.save_state(state)
+        await bot.api.send_text_message(room.room_id, response)    
 
     bot.run()
